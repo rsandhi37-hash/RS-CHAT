6a3f167241ca0bf2217945e294f973f51a9a2d4f
Rehan Sandhi <rsandhi37@gmail.com>
2026-02-16 15:26:29 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'rs-chat-v1';
-const urlsToCache = ['index.html', 'manifest.json'];
+const CACHE_NAME = 'rs-chat-v3';
+const urlsToCache = ['./', 'index.html', 'manifest.json'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)));
