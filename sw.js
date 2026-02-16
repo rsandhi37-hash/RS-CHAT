2ac0c9ecf6d2b612b1bfe3565f268e5061cc1918
Rehan Sandhi <rsandhi37@gmail.com>
2026-02-16 15:11:43 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,34 +1,10 @@
-const CACHE_NAME = 'rs-chat-v2'; // Version changed to v2
-const urlsToCache = [
-  './',
-  'index.html',
-  'manifest.json'
-];
+const CACHE_NAME = 'rs-chat-v1';
+const urlsToCache = ['index.html', 'manifest.json'];
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)));
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(caches.match(event.request).then((response) => response || fetch(event.request)));
 });
